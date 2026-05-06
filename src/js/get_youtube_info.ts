f60cc8853a4423d99abed801a2e2aa94e70e006a
Luke Withrow <lukiewithrow@gmail.com>
2026-05-06 15:39:04 -0400

    usernames using banner.yt, live chat support also displays errors and indicates whether messages are replies/live chats

--- src/js/get_youtube_info.ts
+++ src/js/get_youtube_info.ts
@@ -15,18 +15,16 @@ export async function getVideoTitlesFromIds(ids: string[]) {
     const responseIndices: AwaitingID = {};
 
     for (let i = 0; i < ids.length; i++) {
-        const unfixedId = ids[i];
-        if (!unfixedId) {
+        const id = ids[i];
+        if (!id) {
             continue;
         }
-        const reg = /([\w\-]{11})/i;
-        const match = unfixedId.match(reg);
+        const reg = /^([\w\-]{11})$/;
+        const match = id.match(reg);
         if (!match) {
             result[i] = "";
             continue;
         }
-
-        const id = match[1];
 
         if (!(id in videoTitles)) {
             if (!responseIndices[id]) {
@@ -58,6 +56,57 @@ export async function getVideoTitlesFromIds(ids: string[]) {
     return result;
 }
 
-export async function getVideoTitleFromId(uuid: string) {
-    return (await getVideoTitlesFromIds([uuid]))[0];
+export async function getVideoTitleFromId(id: string) {
+    return (await getVideoTitlesFromIds([id]))[0];
+}
+
+export async function getUsernamesFromIds(ids: string[]) {
+    
+    const storage = localStorage.getItem("usernames");
+    const usernames: IDs = storage ? JSON.parse(storage) : {};
+    const result: string[] = [];
+
+    const responsePromises: Promise<any>[] = [];
+    const responseIndices: AwaitingID = {};
+
+    for (let i = 0; i < ids.length; i++) {
+        const id = ids[i];
+        if (!id) {
+            continue;
+        }
+        const reg = /^[\w\-]{24}$/;
+        const match = id.match(reg);
+        if (!match) {
+            result[i] = "";
+            continue;
+        }
+
+        if (!(id in usernames)) {
+            if (!responseIndices[id]) {
+                responseIndices[id] = [i];
+            } else {
+                responseIndices[id].push(i);
+            }
+
+            responsePromises.push(fetch(`https://banner.yt/api/channel/${id}`).then(async (response) => {
+                if (response.ok) {
+                    const data = await response.json();
+                    usernames[id] = data.handle;
+                } else {
+                    usernames[id] = "";
+                }
+            }).finally(() => {
+                for (const index of responseIndices[id]) {
+                    result[index] = usernames[id] || "";
+                }
+            }));
+        } else {
+            result[i] = usernames[id] || "";
+        }
+    }
+
+    await Promise.allSettled(responsePromises);
+
+    localStorage.setItem("usernames", JSON.stringify(usernames));
+    return result;
 }
--- src/page/youtube_comment/index.ts
+++ src/page/youtube_comment/index.ts
@@ -1,15 +1,22 @@
 import * as info from "@js/get_youtube_info.js";
 
+enum CommentType {
+    Comment,
+    Post,
+    LiveChat,
+}
+
 interface Comment {
     id: string,
-    channelId: string,
+    userId: string,
+    username?: string,
     timestamp: number,
     videoId: string,
     videoTitle?: string,
-    isPost?: boolean,
+    type: CommentType,
     price?: number,
     parentId?: string,
-    topLevelCommentId?: string,
+    topLevelId?: string,
     text: any[]
 }
 
@@ -20,21 +27,53 @@ interface CSV {
 
 const uploadInput = document.getElementById("uploadInput") as HTMLInputElement;
 const resetInput = document.getElementById("resetInput") as HTMLButtonElement;
+const getInput = document.getElementById("getInput") as HTMLInputElement;
 const commentsDiv = document.getElementById("commentsDiv") as HTMLDivElement;
 
+const errorsDiv = document.getElementById("error") as HTMLDivElement;
+let errors: string[] = [];
+
 let comments: Comment[] = [];
+let uniquePosts: string[] = [];
+let uniqueUnknownVideos: string[] = [];
+let uniqueUnknownUsers: string[] = [];
 
 uploadInput.addEventListener("change", async () => {
     loading(true);
-    await updateComments(uploadInput.files, comments);
+    errors = [];
+    await updateComments(uploadInput.files, comments, getInput.checked, errors);
+    errorsPrint();
     commentsPrint();
     loading(false);
 });
 
 resetInput.addEventListener("click", () => {
+    errors = [];
     comments = [];
+    uniquePosts = [];
+    uniqueUnknownVideos = [];
+    uniqueUnknownUsers = [];
+    errorsPrint();
     commentsPrint();
 });
+
+function getUniqueIndex<T>(id: T, source: T[]) {
+    let index = source.indexOf(id);
+    if (index === -1) {
+        index = source.length;
+        source.push(id);
+    }
+    return index;
+}
+
+function errorsPrint() {
+    if (!errors.length) {
+        errorsDiv.setAttribute("hidden", "");
+        return;
+    }
+    errorsDiv.removeAttribute("hidden");
+    errorsDiv.replaceChildren(...errors.map(error => createElementEX("p", {}, [error])));
+}
 
 function commentsPrint() {
     console.log(comments);
@@ -48,22 +87,91 @@ function commentsPrint() {
     commentsDiv.replaceChildren();
 
     for (const comment of comments) {
+
+        const commentBody = comment.text.map((item) => {
+            if (item.videoLink) {
+                const videoLink = item.videoLink;
+                return createElementEX("a", {
+                    "href": `https://youtu.be/${videoLink.externalVideoId}${videoLink.startTimeSeconds ? `&t=${videoLink.startTimeSeconds}` : ""}`,
+                    "target": "_blank"
+                }, [item.text]);
+
+            } else if (item.hashtag) {
+                const hashtag = item.hashtag;
+                return createElementEX("a", {
+                    "href": `https://www.youtube.com/hashtag/${hashtag.hashtagId}`,
+                    "target": "_blank"
+                }, [item.text]);
+
+            } else if (item.mention) {
+                const mention = item.mention;
+                return createElementEX("span", {}, [
+                    createElementEX("a", {
+                        "href": `https://www.youtube.com/channel/${mention.externalChannelId}`,
+                        "target": "_blank"
+                    }, [item.text]),
+                    " "
+                ]);
+            } else if (item.emoji) {
+                const emoji = item.emoji;
+                return createElementEX("img", {
+                    "src": emoji.customEmojiUrl,
+                    "alt": "custom emoji "
+                });
+
+            } else if (item.text === "\n") {
+                return createElementEX("br");
+
+            } else {
+
+                /* const keys = Object.keys(item);
+                if (keys.length > 1) {
+                    console.log(keys);
+                } */
+
+                return item.text;
+            }
+        });
+
+        if (comment.type === CommentType.LiveChat) {
+            commentBody.unshift(createElementEX("div", { "class": "comment-livechat" }, ["🗨 Live Chatted"]));
+        } else if (comment.parentId) {
+            const reply: any[] = [
+                createElementEX("a", {
+                    "href": `https://${comment.type === CommentType.Post ? `www.youtube.com/post/${comment.videoId}?` : `youtu.be/${comment.videoId}&`}lc=${comment.parentId}`,
+                    "target": "_blank"
+                }, ["⤷ Replied"])
+            ];
+            if (comment.parentId !== comment.topLevelId) {
+                reply.push(
+                    " ",
+                    createElementEX("a", {
+                        "href": `https://${comment.type === CommentType.Post ? `www.youtube.com/post/${comment.videoId}?` : `youtu.be/${comment.videoId}&`}lc=${comment.topLevelId}`,
+                        "target": "_blank"
+                    }, ["(Top)"])
+                );
+            }
+            commentBody.unshift(createElementEX("div", { "class": "comment-reply" }, reply));
+        }
+
         const newEl = createElementEX("div", { "class": "comment" }, [
             createElementEX("div", { "class": "comment-header" }, [
                 createElementEX("h3", {}, [
                     createElementEX("a", {
-                        "href": `https://www.youtube.com/channel/${comment.channelId}`,
+                        "href": `https://www.youtube.com/channel/${comment.userId}`,
                         "target": "_blank"
-                    }, [`@${comment.channelId}`]),
+                    }, [comment.username || createElementEX("i", {}, [`Unavailable user #${getUniqueIndex(comment.userId, uniqueUnknownUsers) + 1}`])]),
                     " > ",
                     createElementEX("a", {
-                        "href": `https://${comment.isPost ? "www.youtube.com/post" : "youtu.be"}/${comment.videoId}`,
+                        "href": `https://${comment.type === CommentType.Post ? "www.youtube.com/post" : "youtu.be"}/${comment.videoId}`,
                         "target": "_blank"
-                    }, [comment.videoTitle || createElementEX("i", {}, [comment.videoId])])
+                    }, [comment.type === CommentType.Post ? createElementEX("i", {}, [`Some community post #${getUniqueIndex(comment.videoId, uniquePosts) + 1}`]) :
+                        comment.videoTitle || createElementEX("i", {}, [`Unavailable video #${getUniqueIndex(comment.videoId, uniqueUnknownVideos) + 1}`])
+                    ])
                 ]),
                 createElementEX("span", {}, [
                     createElementEX("a", {
-                        "href": `https://${comment.isPost ? `www.youtube.com/post/${comment.videoId}?` : `youtu.be/${comment.videoId}&`}lc=${comment.id}`,
+                        "href": `https://${comment.type === CommentType.Post ? `www.youtube.com/post/${comment.videoId}?` : `youtu.be/${comment.videoId}&`}lc=${comment.id}`,
                         "target": "_blank"
                     }, [
                         createElementEX("time", { "datetime": `${comment.timestamp}` }, [
@@ -72,74 +180,81 @@ function commentsPrint() {
                     ])
                 ]),
             ]),
-            createElementEX("div", { "class": "comment-body" }, comment.text.map((item) => {
-                if (item.videoLink) {
-                    const videoLink = item.videoLink;
-                    return createElementEX("a", {
-                        "href": `https://youtu.be/${videoLink.externalVideoId}${videoLink.startTimeSeconds ? `&t=${videoLink.startTimeSeconds}` : ""}`,
-                        "target": "_blank"
-                    }, [item.text]);
-
-                } else if (item.hashtag) {
-                    const hashtag = item.hashtag;
-                    return createElementEX("a", {
-                        "href": `https://www.youtube.com/hashtag/${hashtag.hashtagId}`,
-                        "target": "_blank"
-                    }, [item.text]);
-
-                } else if (item.mention) {
-                    const mention = item.mention;
-                    return createElementEX("span", {}, [
-                        createElementEX("a", {
-                            "href": `https://www.youtube.com/channel/${mention.externalChannelId}`,
-                            "target": "_blank"
-                        }, [item.text]),
-                        " "
-                    ]);
-                } else if (item.text === "\n") {
-                    return createElementEX("br");
-                } else {
-                    /* const keys = Object.keys(item);
-                    if (keys.length > 1) {
-                        console.log(keys);
-                    } */
-                    
-                    return item.text;
-                }
-            }))
+            createElementEX("div", { "class": "comment-body" }, commentBody)
         ]);
         commentsDiv.appendChild(newEl);
     }
 }
 
-async function updateComments(uploadedFiles: FileList | null, comments: Comment[]) {
+async function updateComments(uploadedFiles: FileList | null, comments: Comment[], getData: boolean, errors: string[]) {
     if (!uploadedFiles) return;
     const files = Array.from(uploadedFiles);
     const newComments: Comment[] = [];
 
-    await Promise.allSettled(files.map(file => promiseFileAsText(file).then((text) => {
+    const results = await Promise.allSettled(files.map(file => promiseFileAsText(file).then((text) => {
         if (!text) return;
         insertComments(newComments, parseCommentCSV(text));
     })));
+    for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        if (result.status === "rejected") {
+            const error = `File \"${uploadedFiles[i].name}\" failed: ${result.reason}`;
+            console.error(error);
+            errors.push(error);
+        }
+    }
 
-    const videoTitles = await info.getVideoTitlesFromIds(newComments.map(comment => comment.videoId));
-    for (let i = 0; i < videoTitles.length; i++) {
-        newComments[i].videoTitle = videoTitles[i];
+    if (getData) {
+        await Promise.allSettled([
+            info.getVideoTitlesFromIds(newComments.map(comment => comment.videoId)).then(videoTitles => {
+                for (let i = 0; i < videoTitles.length; i++) {
+                    if (videoTitles[i]) {
+                        newComments[i].videoTitle = videoTitles[i];
+                    }
+                }
+            }),
+            info.getUsernamesFromIds(newComments.map(comment => comment.userId)).then(usernames => {
+                for (let i = 0; i < usernames.length; i++) {
+                    if (usernames[i]) {
+                        newComments[i].username = usernames[i];
+                    } 
+                }
+            })
+        ])
     }
     insertComments(comments, newComments);
 }
 
 function parseCommentCSV(text: string) {
-    const csv = parseCSV(text, ["Comment ID", "Channel ID", "Comment Create Timestamp", "Comment Text"]);
+    const csv = parseCSV(text);
+
+    if (!csv.fields["Channel ID"]) {
+        throw new TypeError("CSV is missing \"Channel ID\" field.");
+    }
+
+    let comment: string;
+    let isLiveChat: boolean;
+    if (csv.fields["Comment ID"] && csv.fields["Comment Create Timestamp"] && csv.fields["Comment Text"]) {
+        comment = "Comment";
+        isLiveChat = false;
+    } else if (csv.fields["Live Chat ID"] && csv.fields["Live Chat Create Timestamp"] && csv.fields["Live Chat Text"]) {
+        comment = "Live Chat";
+        isLiveChat = true;
+    } else {
+        throw new TypeError("CSV is missing required comment fields or live chat fields.");
+    }
+    const commentText = `${comment} Text`;
+    const commentCreateTimestamp = `${comment} Create Timestamp`;
+    const commentId = `${comment} ID`;
 
     if (!csv.fields["Post ID"] && !csv.fields["Video ID"]) {
-        throw new TypeError("CSV has neither a \"Post ID\" field nor a \"Video ID\" field.");
+        throw new TypeError("CSV is missing a \"Post ID\" field or a \"Video ID\" field.");
     }
 
     const newComments: Comment[] = [];
 
     for (let i = 0; i < csv.count; i++) {
-        const textJson = csv.fields["Comment Text"][i];
+        const textJson = csv.fields[commentText][i];
         let text: any[];
         try {
             text = JSON.parse(`[${textJson}]`);
@@ -148,11 +263,11 @@ function parseCommentCSV(text: string) {
             text = [];
         }
 
-        const timestamp = Date.parse(csv.fields["Comment Create Timestamp"][i]);
+        const timestamp = Date.parse(csv.fields[commentCreateTimestamp][i]);
 
         let videoId: string;
         let isPost: boolean;
-        if (csv.fields["Post ID"] && csv.fields["Post ID"][i]) {
+        if (!isLiveChat && csv.fields["Post ID"] && csv.fields["Post ID"][i]) {
             videoId = csv.fields["Post ID"][i];
             isPost = true;
         } else {
@@ -161,15 +276,12 @@ function parseCommentCSV(text: string) {
         }
 
         newComments[i] = {
-            id: csv.fields["Comment ID"][i],
+            id: csv.fields[commentId][i],
             videoId: videoId.trim(),
-            channelId: csv.fields["Channel ID"][i],
+            userId: csv.fields["Channel ID"][i],
             timestamp: timestamp,
+            type: isLiveChat ? CommentType.LiveChat : isPost ? CommentType.Post : CommentType.Comment,
             text: text
-        }
-
-        if (isPost) {
-            newComments[i].isPost = true;
         }
     }
 
@@ -195,7 +307,7 @@ function parseCommentCSV(text: string) {
         for (let i = 0; i < csv.count; i++) {
             const x = csv.fields["Top-Level Comment ID"][i];
             if (x) {
-                newComments[i].topLevelCommentId = x;
+                newComments[i].topLevelId = x;
             }
         }
     }
@@ -246,14 +358,14 @@ function* parseCSVRow(row: string) {
     }
 }
 
-function parseCSV(text: string, requiredFields?: string[]): CSV {
+function parseCSV(text: string, /*requiredFields?: string[]*/): CSV {
     let rows = text.split("\n");
 
     let count = rows.length - 1;
     for (; count > 1 && !rows[count]; count--);
 
     const header = [...parseCSVRow(rows[0])];
-    if (requiredFields) {
+    /* if (requiredFields) {
         const missingFields: string[] = []
         for (const field of requiredFields) {
             if (!header.includes(field)) {
@@ -263,7 +375,7 @@ function parseCSV(text: string, requiredFields?: string[]): CSV {
         if (missingFields.length > 0) {
             throw new TypeError(`CSV missing required fields: \"${missingFields.join("\", \"")}\"`);
         }
-    }
+    } */
 
     let fields: { [key: string]: string[] } = {};
     for (const field of header) {
